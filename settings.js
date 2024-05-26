4f8038f74a4539ed5cd1e9b4c15495092a71237a
Maelstrom2x <166751759+Maelstrom2x@users.noreply.github.com>
2024-05-26 18:48:11 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: @IRONWILL786" //ur yt chanel name
+global.socialm = "GitHub: Sadistic_xc" //ur github or insta name
+global.location = "Pakistan, Punjab, Islamabad" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.botname = 'RED REAPER' //ur bot name
+global.ownernumber = 923140152133' //ur owner number
+global.ownername = 'NOT ASIM MUNEER' //ur owner name
+global.websitex = "https://www.instagram.com/sadistic_xc?igsh=M2xreHJlNzQyZGI1"
+global.wagc = "https://chat.whatsapp.com/CjeoUKPRTEvHGUx0BMvhHG"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "ASIM MUNEER."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = "PROPERTY OF"
+global.author = "NOT ASIM MUNEER\n\n+923140152133"
+global.creator = "923140152133@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["923140152133"] // Premium User
 global.hituet = 0
 
 //bot sett
